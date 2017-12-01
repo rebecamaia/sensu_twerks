dcb5b512f444d934d150f5f03183d02bdca148c0
Rebeca Maia <rebecamaia.lmp@gmail.com>
2017-12-01 16:32:26 -0300

    Correcting grep method.

--- plugins/check-jboss.sh
+++ plugins/check-jboss.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 INS=$1
-RES=`ps axu | grep $INS | grep java | grep -v grep | cut -d " " -f3`
+RES=`ps axu | grep $INS | grep java | grep -v grep | tr -s " " | cut -d " " -f2`
 
 if [ "${RES:-null}" = null ]; then
   echo "WARNING: JBOSS might not be running for instance $1!"
-  exit 0
+  exit 2
 else
   echo "OK: JBOSS is running for is running for instance $1 with PID $RES"
-  exit 1
+  exit 0
 fi
 echo "Unknown: How did I get here?"
 exit 3
