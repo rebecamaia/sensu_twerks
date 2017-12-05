64de1aa0d705be5509b26560727af898eb760003
Rebeca Maia <rebecamaia.lmp@gmail.com>
2017-12-05 10:08:22 -0300

    Adding feeder to grab information from sensu and send it to grafana's single stat panel.

---
+++ handlers/feeder.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+from sensu_plugin import SensuHandler
+
+class InfluxFeeder(SensuHandler):
+    def handle(self):
+        target_client = self.event['client']['name']
+        check_result = self.event['check']['status']
+        payload = 'vpn-process,host=%s value=%s' % (target_client,check_result)
+        url = 'http://sensu.esig.com.br:8086/write?db=grafana'
+        r = requests.post(url,payload)
+        print(r)
+
+InfluxFeeder()
